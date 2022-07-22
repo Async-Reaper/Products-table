39da12d8d38f1ccc028be41ec22ecb3fb088945d
unknown <m_igor_r@mail.ru>
2022-07-22 10:30:41 +0700

    create func for set limit

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,13 +1,22 @@
 import { FormControl, InputLabel, NativeSelect } from '@mui/material'
 import React, { FC } from 'react'
+import { useTypedDispatch } from '../../hooks/useTypedDispatch'
+import { setLimit } from '../../store/reducers/paginationReducer'
 
 const Select: FC = () => {
+   const dispatch = useTypedDispatch()
+
+   const selectHandle = (e: React.ChangeEvent<HTMLSelectElement>) => {
+      dispatch(setLimit(e.target.value))
+   }
+
    return (
       <FormControl>
          <InputLabel variant="standard" htmlFor="uncontrolled-native">
             По
          </InputLabel>
          <NativeSelect
+            onChange={e => selectHandle(e)}
             defaultValue={5}
             inputProps={{
                name: 'age',
